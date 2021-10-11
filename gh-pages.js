045a5ec9b4bf6245af59fd8d91e3f75708db6848
aditya-xq <adityavivek1998@gmail.com>
2021-10-11 05:50:21 +0530

    test

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
 	'public', // path to public directory
 	{
 		branch: 'gh-pages',
-		repo: 'https://github.com/aditya-xq/aditya-xq.github.io', // Update to point to your repository
+		repo: 'https://github.com/aditya-xq/aditya-xq.github.io.git', // Update to point to your repository
 		user: {
 			name: 'aditya-xq', // update to use your name
 			email: 'adityavivek1998@gmail.com' // Update to use your email
