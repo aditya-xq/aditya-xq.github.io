38547fe8193acabbc43f8c19496c7fca4ce2cd50
aditya-xq <adityavivek1998@gmail.com>
2021-10-11 05:39:46 +0530

    deploy build

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+	'public', // path to public directory
+	{
+		branch: 'gh-pages',
+		repo: 'https://github.com/aditya-xq/aditya-xq.github.io', // Update to point to your repository
+		user: {
+			name: 'aditya-xq', // update to use your name
+			email: 'adityavivek1998@gmail.com' // Update to use your email
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
+);
