4df8a3d03b295575903d55100d9e7448ee7700ff
aditya-xq <adityavivek1998@gmail.com>
2025-05-22 00:20:20 +0530

    chore: altcss version update

--- build.js
+++ build.js
@@ -1,34 +1,34 @@
-import { minify } from 'html-minifier';
-import fs from 'fs';
-import path from 'path';
+import { minify } from 'html-minifier'
+import fs from 'fs'
+import path from 'path'
 
 // Paths
-const SRC_DIR = './src';
-const DIST_DIR = './dist';
-const CSS_SOURCE = './node_modules/altcss/alt.min.css';
-const CSS_DEST = path.join(DIST_DIR, 'alt.min.css');
-const ASSETS_SOURCE = path.join(SRC_DIR, 'assets');
-const ASSETS_DEST = path.join(DIST_DIR, 'assets');
+const SRC_DIR = './src'
+const DIST_DIR = './dist'
+const CSS_SOURCE = './node_modules/altcss/alt.min.css'
+const CSS_DEST = path.join(DIST_DIR, 'alt.min.css')
+const ASSETS_SOURCE = path.join(SRC_DIR, 'assets')
+const ASSETS_DEST = path.join(DIST_DIR, 'assets')
 
 // Step 1: Clean the output directory
-fs.rmSync(DIST_DIR, { recursive: true, force: true });
-fs.mkdirSync(DIST_DIR);
-fs.mkdirSync(ASSETS_DEST);
+fs.rmSync(DIST_DIR, { recursive: true, force: true })
+fs.mkdirSync(DIST_DIR)
+fs.mkdirSync(ASSETS_DEST)
 
 // Step 2: Optimize and Copy HTML
-const htmlPath = path.join(SRC_DIR, 'index.html');
-const htmlContent = fs.readFileSync(htmlPath, 'utf-8');
+const htmlPath = path.join(SRC_DIR, 'index.html')
+const htmlContent = fs.readFileSync(htmlPath, 'utf-8')
 const minifiedHTML = minify(htmlContent, {
   collapseWhitespace: true,
   removeComments: true,
   minifyCSS: true,
-});
-fs.writeFileSync(path.join(DIST_DIR, 'index.html'), minifiedHTML);
+})
+fs.writeFileSync(path.join(DIST_DIR, 'index.html'), minifiedHTML)
 
 // Step 3: Copy CSS from node_modules
-fs.copyFileSync(CSS_SOURCE, CSS_DEST);
+fs.copyFileSync(CSS_SOURCE, CSS_DEST)
 
 // Step 4: Copy assets
-fs.cpSync(ASSETS_SOURCE, ASSETS_DEST, { recursive: true });
+fs.cpSync(ASSETS_SOURCE, ASSETS_DEST, { recursive: true })
 
-console.log('Build completed successfully!');
+console.log('Build completed successfully!')
